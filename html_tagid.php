327944eac582819faaef743c74e52ce84e9cf89d
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-07 22:39:24 +0300

    Last word <dt>

--- html_tagid.php
+++ html_tagid.php
@@ -2,23 +2,95 @@
 <!DOCTYPE html>
 <!--HTML elemendid (v.a need, mis pole toetatud HTML5-s) -->
 <head>
-
+    <base href="ASSETS/" target="_blank">
+    <link type="text/css" rel="stylesheet" href="css_prj2.css"/>
     <title>
 
     </title>
 </head>
 <body>
 
+<address>
+    Saada mulle kiri: <a href="mailto:kadri.mihhailova@khk.ee"><b>Kadri Mihhailova</b></a>.<br>
+    või külasta kooli kodulehte:<br>
+    <a href="http://khk.ee/"><abbr title="Tartu Kutsehariduskeskus">TKHK</abbr></a><br>
+    Tartu, Eesti<br>
+</address>
+
+<article>
+    <h1>See on jänku.</h1>
+    <p>Silmale vajutades saad <bdi> jänesesilma </bdi> devianarti, ninale vajutades saad jänkukese nina liigutuste seletuse..</p>
+</article>
+
+<img src="pictures/janku.gif" width="140" height="150" usemap="#bunny">
+
+<map name="bunny">
+    <area shape="circle" coords="95,25,10" href="http://rabbiteyes.deviantart.com/" alt="eye">
+    <area shape="circle" coords="60,46,10" href="http://language.rabbitspeak.com/follow-my-nose/" alt="nose">
+</map>
+<aside>
+        <h2>Küüliku keskmine eluiga on 5-8 aastat ning eluea pikkust saab pikendada küüliku steriliseerimise/kastreerimise teel.</h2>
+        <h3>Küülik ehk kodujänes (Oryctolagus cuniculus) on jäneslaste sugukonda kuuluv imetaja.</h3>
+</aside>
+
+<div id="audio_div"></div> <audio controls>
+    <source src="audio/18 Lookin' Out My Back Door.mp3" type="audio/mpeg">
+
+</audio></div>
+
+
+<h4>Sõna kirik on tagurpidi ka <bdo dir="rtl">kirik</bdo></h4>
+<h5>Aga minu nimi Kadri on tagurpidi <bdo dir="rtl">Kadri</bdo></h5>
+
+
+<h6><cite>„Varas“</cite> autor: Göran Tunströmi</h6>
+<blockquote cite="https://noiaelu.wordpress.com/2007/02/05/varas-varas-varas/">
+    Johan on kolmeteistkümnes, kasulaps kaltsakaperes, väike tõsine olend keset räppa, joomist, kaklusi, koduterrorit, nõrgamõistuslikke juhme sugulasi.
+    <br>
+    Peaaegu üksi, kui mitte arvestada onutütar Hedvigit, teist väikest tõsiduselast. Seltsilist, trööstijat, armastatut.
+</blockquote>
+<button type="button">Click Me!</button>
+<canvas id="myCanvas">See on minu canvas, kuid kahjuks puudub mul JS fail..</canvas>
+
+<code>See on arvutikood.</code>
+
+<datalist id="berries">
+    <option value="Blueberry">
+    <option value="Rasberry">
+    <option value="Strawberry">
+    <option value="Cherry">
+</datalist>
+
+<dl>
+    <dt>Paber</dt>
+    <dd>Valge ja sodimata.</dd>
+    <dt>Sodipaber</dt>
+    <dd>Enamasti pliiatsiga täis soditud.</dd>
+</dl>
+
+<p>Arvad, et mu tuju on <del>hea</del> <ins>halb</ins>?</p>
+
+<details>
+    <summary>Ava mind, kui tahad näha, mis siin sees on ;)</summary>
+    <p> Tüng! Tegelikult pole ühtegi detaili.</p>
+</details>
+
+<p>Defineerin <dfn>KÜ</dfn>- Korterühistu</p>
+
+<dialog open>Olen tulevikus kasulik kas hüpikakna või muu kasulikuna, et teistega suhelda. Jään teiste peale.</dialog>
+<br>
+<br>
+<br>
 
 
 
+
+TABEL:
+
+<col>	Specifies column properties for each column within a <colgroup> element
+<colgroup>	Specifies a group of one or more columns in a table for formatting
+
+<caption>	Defines a table caption
 </body>
-
-
-
-
-
-
-
 
 </html>
