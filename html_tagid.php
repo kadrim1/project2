4f4ab92c7d3b30b6340982d63b6f69446db1ebb0
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-07 20:23:09 +0300

    Esimene proov.

---
+++ html_tagid.php
@@ -0,0 +1,24 @@
+<html>
+<!DOCTYPE html>
+<!--HTML elemendid (v.a need, mis pole toetatud HTML5-s) -->
+<head>
+
+    <title>
+
+    </title>
+</head>
+<body>
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+</html>
